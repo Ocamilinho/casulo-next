29a66d914ee8b862cd8277c45a845a09990537db
Adriano Camilo <adrianocamilo055@gmail.com>
2025-03-07 20:09:10 -0300

    feat: Desagregando contatos (public)

--- app/contato/page.tsx
+++ app/contato/page.tsx
@@ -1,7 +1,9 @@
 import Divider from "@/components/Divider";
-import Footer from "@/components/Footer"
-import Header from "@/components/Header"
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 import SocialLink from "@/components/contato/SocialLink";
+import {ContatoData} from "@/public/contato"
+
 export default function Contato() {
     return (
         <>
@@ -13,14 +15,9 @@ export default function Contato() {
                         <Divider />
                         <p className="text-xl text-branco text-center">Procuramos articular e divulgar as nossas ações político-pedagógicas através das redes sociais. Caso queira entrar em contato para nos conhecer melhor, seja enquanto interesse pessoal, para somar aos nossos trabalhos, ou visando à construção de algum projeto de caráter coletivo e/ou comunitário, sinta-se à vontade!</p>
                         <div className="flex gap-3 flex-wrap justify-center">
-                            <SocialLink href="https://www.facebook.com/groups/721949191548369/" platform="facebook" />
-                            <SocialLink href="mailto:contato@coletivocasulo.com?subject=Nome - Assunto" platform="email" />
-                            <SocialLink href="https://wa.me/5522999066017" platform="whatsapp" />
-                            <SocialLink href="https://bsky.app/profile/casulogepecd.bsky.social" platform="bluesky" />
-                            <SocialLink href="https://www.instagram.com/casulo.gepecd/" platform="instagram" />
-                            <SocialLink href="https://www.youtube.com/@coletivocasulo8559" platform="youtube" />
-                            <SocialLink href="https://www.threads.net/@casulo.gepecd" platform="threads" />
-                            <SocialLink href="https://x.com/CasuloColetivo" platform="twitter" />
+                            {ContatoData.map((item, index) => (
+                                <SocialLink key={index} href={item.link} platform={item.platform} />
+                            ))}
                         </div>
                     </div>
                 </div>
@@ -29,3 +26,4 @@ export default function Contato() {
         </>
     );
 }
+
---
+++ public/contato.tsx
@@ -0,0 +1,34 @@
+export const ContatoData = [
+    {
+        link: "https://www.facebook.com/groups/721949191548369/",
+        platform: "facebook",
+    },
+    {
+        link: "mailto:contato@coletivocasulo.com?subject=Nome - Assunto",
+        platform: "email",
+    },
+    {
+        link: "https://wa.me/5522999066017",
+        platform: "whatsapp",
+    },
+    {
+        link: "https://bsky.app/profile/casulogepecd.bsky.social",
+        platform: "bluesky",
+    },
+    {
+        link: "https://www.instagram.com/casulo.gepecd/",
+        platform: "instagram",
+    },
+    {
+        link: "https://www.youtube.com/@coletivocasulo8559",
+        platform: "youtube",
+    },
+    {
+        link: "https://www.threads.net/@casulo.gepecd",
+        platform: "threads",
+    },
+    {
+        link: "https://x.com/CasuloColetivo",
+        platform: "twitter",
+    },
+];
