41fa3f98109fbb21bc7c9dc26295554b21c264be
Adriano Camilo <adrianocamilo055@gmail.com>
2025-05-22 11:18:47 -0300

    feat: Inclusão da renderização estática

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+};
